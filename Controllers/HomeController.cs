838ca9013ffc8bfd0c46f8e1cb87db4bed39d3f3
f-Rra <facundo.rra@gmail.com>
2026-09-01 17:46:19 -0300

    chore(app): crear el proyecto f4872
    - Esqueleto MVC en la raiz, con la solucion para abrir desde Visual Studio
    - Sacada la pagina de Privacy, su enlace y el logger
    - Layout en espanol con la marca 48·72 y una vista provisoria

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using f4872.Models;
+
+namespace f4872.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
